928faf4dc9a93869ef25a5bf0b6ed2a4faf27cc9
Nouu <noelarrieulacoste@yahoo.fr>
2021-01-21 22:02:23 +0100

    declare all needed methods in FactionManager

--- src/main/java/org/esgi/el_presidente/core/factions/FactionManager.java
+++ src/main/java/org/esgi/el_presidente/core/factions/FactionManager.java
@@ -1,4 +1,59 @@
 package org.esgi.el_presidente.core.factions;
 
+import java.util.List;
+
 public class FactionManager {
+    private List<Faction> factionList;
+
+    public FactionManager() {
+    }
+
+    public double getGlobalSatisfaction() {
+        return 0;
+    }
+
+    public void initFactionList(int initialSatisfaction, int initialPartisans, int initialLoyalistSatisfaction, int initialLoyalistPartisan) {
+
+    }
+
+    public List<Faction> getFactionList() {
+        return null;
+    }
+
+    public Faction getFaction(FactionType factionType) {
+        return null;
+    }
+
+    public int getTotalPartisan() {
+        return 0;
+    }
+
+    public void addFactionSatisfaction(FactionType factionType, int statisfaction) {
+
+    }
+
+    public void addAllFactionSatisfaction(int statisfaction) {
+
+    }
+
+    public void addFactionPartisan(FactionType factionType, int partisans) {
+
+    }
+
+    public void addFactionPartisanPercent(FactionType factionType, int partisansPercent) {
+
+    }
+
+    public void addAllFactionsPartisan(int partisans) {
+
+    }
+
+    public void addAllFactionsPartisanPercent(int partisansPercent) {
+
+    }
+
+    public void removeRandomlyFactionPartisans(int partisans) {
+
+    }
+
 }
