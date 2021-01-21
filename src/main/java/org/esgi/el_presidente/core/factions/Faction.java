d426763605c40f291ca082bb0efe8c658747be86
Nouu <noelarrieulacoste@yahoo.fr>
2021-01-21 21:39:20 +0100

    Faction class

--- src/main/java/org/esgi/el_presidente/core/factions/Faction.java
+++ src/main/java/org/esgi/el_presidente/core/factions/Faction.java
@@ -1,4 +1,41 @@
 package org.esgi.el_presidente.core.factions;
 
 public class Faction {
+    private int statisfaction;
+    private int partisans;
+    private final FactionType factionType;
+
+    public Faction(FactionType factionType, int statisfaction, int partisans) {
+        this.factionType = factionType;
+        this.statisfaction = Math.max(statisfaction, 0);
+        this.partisans = Math.max(partisans, 0);
+    }
+
+    public FactionType getFactionType() {
+        return factionType;
+    }
+
+    public int getStatisfaction() {
+        return statisfaction;
+    }
+
+    public int getPartisans() {
+        return partisans;
+    }
+
+    public void addStatisfaction(int statisfaction) {
+        // Si une factiontombe à 0% de satisfaction, alors il ne sera plus possible de remonter ce pourcentage
+        if (this.statisfaction > 0) {
+            this.statisfaction = Math.max(Math.min(this.statisfaction + statisfaction, 100), 0);
+        }
+    }
+
+    public void addPartisansPercent(int percent) {
+        double diff = (double) partisans * ((double) percent / 100);
+        this.partisans += (int) diff;
+    }
+
+    public void addPartisans(int partisans) {
+        this.partisans = Math.max(0, this.partisans + partisans);
+    }
 }
