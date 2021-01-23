694e2c75ac497d15840b0e50f68abef22b798f2c
Nouu <noelarrieulacoste@yahoo.fr>
2021-01-23 18:24:08 +0100

    FactionType test

---
+++ src/test/java/org/esgi/el_presidente/FactionTypeTest.java
@@ -0,0 +1,29 @@
+package org.esgi.el_presidente;
+
+import org.assertj.core.api.Assertions;
+import org.esgi.el_presidente.core.factions.FactionType;
+import org.junit.Test;
+
+public class FactionTypeTest {
+
+    @Test
+    public void testFactionTypeLoyalistExists() {
+        Assertions.assertThat(FactionType.loyalist).isNotNull();
+    }
+
+    @Test
+    public void testFactionTypeToString() {
+        Assertions.assertThat(FactionType.loyalist.toString()).isEqualTo("loyalistes");
+    }
+
+    @Test
+    public void testGetFactionTypeFromString() {
+        Assertions.assertThat(FactionType.valueOf("loyalist")).isEqualTo(FactionType.loyalist);
+    }
+
+    @Test
+    public void testGetInexistantFactionTypeFromString() {
+        Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> FactionType.valueOf("inexistant"));
+    }
+}
