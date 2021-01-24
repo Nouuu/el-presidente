8e54d79efdbd67442fa36811f2918e6ceeb96cbf
Nouu <noelarrieulacoste@yahoo.fr>
2021-01-24 01:46:24 +0100

    pass all tests

--- src/main/java/org/esgi/el_presidente/core/season/Season.java
+++ src/main/java/org/esgi/el_presidente/core/season/Season.java
@@ -1,4 +1,19 @@
 package org.esgi.el_presidente.core.season;
 
 public enum Season {
+    spring("printemps"),
+    summer("été"),
+    autumn("automne"),
+    winter("hiver");
+
+    private final String season;
+
+    Season(String season) {
+        this.season = season;
+    }
+
+    @Override
+    public String toString() {
+        return season;
+    }
 }
