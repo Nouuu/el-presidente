fc31b7bf437321d020d6cfa61fda841bc20d1f66
Nouu <noelarrieulacoste@yahoo.fr>
2021-01-24 01:44:58 +0100

    testSeasonsCount4

--- src/test/java/org/esgi/el_presidente/SeasonTest.java
+++ src/test/java/org/esgi/el_presidente/SeasonTest.java
@@ -1,4 +1,10 @@
 package org.esgi.el_presidente;
 
 public class SeasonTest {
+
+    @Test
+    public void testSeasonsCount4() {
+        Assertions.assertThat(Arrays.stream(Season.values()).count()).isEqualTo(4);
+    }
+
 }
