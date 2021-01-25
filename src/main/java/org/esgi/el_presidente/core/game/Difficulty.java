aef848d85346ff274daf92ec67c884f87cf6f2c9
Noe Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2021-01-25 15:07:18 +0100

    pass all tests

--- src/main/java/org/esgi/el_presidente/core/game/Difficulty.java
+++ src/main/java/org/esgi/el_presidente/core/game/Difficulty.java
@@ -1,15 +1,31 @@
 package org.esgi.el_presidente.core.game;
 
 public enum Difficulty {
-    ;
+    EASY("facile", 1.5, 0.75),
+    MEDIUM("normal", 1, 1),
+    HARD("difficile", 0.75, 1.5),
+    HARDCORE("niveau JDG", 0.5, 2);
 
-    private String name;
-    private double gainMultiplier;
-    private double loseMultiplier;
+    private final String name;
+    private final double gainMultiplier;
+    private final double loseMultiplier;
 
     Difficulty(String name, double gainMultiplier, double loseMultiplier) {
         this.name = name;
         this.gainMultiplier = gainMultiplier;
         this.loseMultiplier = loseMultiplier;
+    }
+
+    public double getGainMultiplier() {
+        return gainMultiplier;
+    }
+
+    public double getLoseMultiplier() {
+        return loseMultiplier;
+    }
+
+    @Override
+    public String toString() {
+        return name;
     }
 }
