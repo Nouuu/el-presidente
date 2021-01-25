f5ac9673945b8a1e6aac4c4b4820b7ff0d50d24a
Noe Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2021-01-25 14:50:01 +0100

    init difficulty test

---
+++ src/test/java/org/esgi/el_presidente/DifficultyTest.java
@@ -0,0 +1,30 @@
+package org.esgi.el_presidente;
+
+import org.junit.Test;
+
+public class DifficultyTest {
+
+    @Test
+    public void testDifficultyEasyExists() {
+    }
+
+    @Test
+    public void testDifficultyMediumExists() {
+    }
+
+    @Test
+    public void testDifficultyHardExists() {
+    }
+
+    @Test
+    public void testDifficultyToString() {
+    }
+
+    @Test
+    public void testDifficultyGetGainMultiplier() {
+    }
+
+    @Test
+    public void testDifficultyGetLoseMultiplier() {
+    }
+}
