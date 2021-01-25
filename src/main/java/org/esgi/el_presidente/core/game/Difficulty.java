e0bb3dae69fd77d950b89bce294b259b0f8630a1
Noe Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2021-01-25 14:44:38 +0100

    set enum attributes

--- src/main/java/org/esgi/el_presidente/core/game/Difficulty.java
+++ src/main/java/org/esgi/el_presidente/core/game/Difficulty.java
@@ -1,2 +1,15 @@
-package org.esgi.el_presidente.core.game;public enum Difficulty {
+package org.esgi.el_presidente.core.game;
+
+public enum Difficulty {
+    ;
+
+    private String name;
+    private double gainMultiplier;
+    private double loseMultiplier;
+
+    Difficulty(String name, double gainMultiplier, double loseMultiplier) {
+        this.name = name;
+        this.gainMultiplier = gainMultiplier;
+        this.loseMultiplier = loseMultiplier;
+    }
 }
