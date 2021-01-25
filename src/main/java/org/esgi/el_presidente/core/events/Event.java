d34ae8ed952d4bd2d3b351143c761b4f2eb96fcb
Nouu <noelarrieulacoste@yahoo.fr>
2021-01-25 21:21:35 +0100

    begin to set attributes

--- src/main/java/org/esgi/el_presidente/core/events/Event.java
+++ src/main/java/org/esgi/el_presidente/core/events/Event.java
@@ -3,6 +3,8 @@ package org.esgi.el_presidente.core.events;
 import org.esgi.el_presidente.core.season.Season;
 
 public class Event {
-   
+    private final Season season;
+    private final String eventDetails;
+
 
 }
