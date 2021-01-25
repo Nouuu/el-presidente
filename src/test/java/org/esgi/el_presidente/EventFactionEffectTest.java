dddfee56970336d58a1c5ab61578bf2f6e34a4a7
Nouu <noelarrieulacoste@yahoo.fr>
2021-01-25 21:10:10 +0100

    generate unit test

---
+++ src/test/java/org/esgi/el_presidente/EventFactionEffectTest.java
@@ -0,0 +1,24 @@
+package org.esgi.el_presidente;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EventFactionEffectTest {
+
+    @Test
+    public void getFactionType() {
+    }
+
+    @Test
+    public void getPartisansPercentEffect() {
+    }
+
+    @Test
+    public void getSatisfactionEffect() {
+    }
+
+    @Test
+    public void testToString() {
+    }
+}
