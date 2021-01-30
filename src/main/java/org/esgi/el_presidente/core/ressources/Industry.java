b3b326f3c2aa6d382333737319511b4f36be7e06
Swann <swann@devloup.dev>
2021-01-30 13:27:38 +0100

    rename industry

--- src/main/java/org/esgi/el_presidente/core/ressources/Industry.java
+++ src/main/java/org/esgi/el_presidente/core/ressources/Industry.java
@@ -1,15 +1,23 @@
 package org.esgi.el_presidente.core.ressources;
 
-public class Indsutry {
+public class Industry {
   private double partOfIsland;
   private final double moneyProductionCoefficient;
 
-  public Indsutry(double partOfIsland, double moneyProductionCoefficient) {
+  public Industry(double partOfIsland, double moneyProductionCoefficient) {
     this.partOfIsland = partOfIsland;
     this.moneyProductionCoefficient = moneyProductionCoefficient;
   }
 
   public double yearlyProductionOfMoney() {
     return partOfIsland * moneyProductionCoefficient;
+  }
+
+  public void grow(double additionalSize) {
+    partOfIsland += additionalSize;
+  }
+
+  public double getSize() {
+    return partOfIsland;
   }
 }
