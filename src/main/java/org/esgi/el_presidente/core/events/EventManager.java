4fb709f38fb7094bb17c7ee81939a49d6449257a
Nouu <noelarrieulacoste@yahoo.fr>
2021-02-09 19:27:02 +0100

    begin event manager

---
+++ src/main/java/org/esgi/el_presidente/core/events/EventManager.java
@@ -0,0 +1,47 @@
+package org.esgi.el_presidente.core.events;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Random;
+
+public class EventManager {
+    private final List<Event> events;
+    private int step;
+    private final Random random;
+
+    public EventManager(List<Event> events) {
+        this.events = Objects.requireNonNullElseGet(events, ArrayList::new);
+        step = 0;
+        random = new Random();
+    }
+
+    public EventManager addEvent(Event event) {
+        events.add(event);
+        return this;
+    }
+
+    public Event getRandomEvent() {
+        if (events.size() == 0) {
+            return null;
+        }
+        return events.get(random.nextInt(events.size()));
+    }
+
+    public Event getNextEvent() {
+        if (events.size() == 0) {
+            return null;
+        }
+        Event event = events.get(step);
+        step = (step + 1) % events.size();
+        return event;
+    }
+
+    public List<Event> getEvents() {
+        return events;
+    }
+
+    public int getStep() {
+        return step;
+    }
+}
