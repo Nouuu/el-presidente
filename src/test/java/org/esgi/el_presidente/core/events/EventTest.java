2a745ba3bb113e27adcfcfa3a84ce1b3758a9323
Nouu <noelarrieulacoste@yahoo.fr>
2021-02-14 18:09:02 +0100

    change event test to handle choices

--- src/test/java/org/esgi/el_presidente/core/events/EventTest.java
+++ src/test/java/org/esgi/el_presidente/core/events/EventTest.java
@@ -3,81 +3,63 @@ package org.esgi.el_presidente.core.events;
 import org.assertj.core.api.Assertions;
 import org.esgi.el_presidente.core.factions.FactionType;
 import org.esgi.el_presidente.core.season.Season;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class EventTest {
 
-    @Test
-    public void addfactionEffect() {
-        EventFactionEffect eventFactionEffect = new EventFactionEffect(FactionType.capitalist, 1, 2);
+    private final EventFactionEffect eventFactionEffect = new EventFactionEffect(FactionType.capitalist, 1, 2);
+    private final EventFactionEffect eventFactionEffect2 = new EventFactionEffect(null, 1, 2);
+    private final List<EventFactionEffect> eventFactionEffects = new ArrayList<>();
+    private final EventChoice eventChoice = new EventChoice("choix", eventFactionEffects);
+    private final List<EventChoice> eventChoices = new ArrayList<>();
+    private final Season season = Season.spring;
+    private final String eventDescription = "lorem ipsum";
+    private final Event event = new Event(season, eventDescription, eventChoices);
 
-        Event event = new Event(Season.autumn, "", null).addfactionEffect(eventFactionEffect);
-
-        Assertions.assertThat(event.getFactionEffects()).hasSize(1).containsExactly(eventFactionEffect);
+    @Before
+    public void setUp() {
+        eventFactionEffects.clear();
+        eventFactionEffects.add(eventFactionEffect);
+        eventFactionEffects.add(eventFactionEffect2);
+        eventChoices.clear();
+        eventChoices.add(eventChoice);
     }
+
 
     @Test
     public void getSeason() {
-        EventFactionEffect eventFactionEffect = new EventFactionEffect(FactionType.capitalist, 1, 2);
-        List<EventFactionEffect> eventFactionEffects = new ArrayList<>();
-        eventFactionEffects.add(eventFactionEffect);
-        Season season = Season.spring;
-
-        Event event = new Event(season, "", eventFactionEffects);
 
         Assertions.assertThat(event.getSeason()).isEqualTo(season);
     }
 
     @Test
     public void getEventDetails() {
-        EventFactionEffect eventFactionEffect = new EventFactionEffect(FactionType.capitalist, 1, 2);
-        List<EventFactionEffect> eventFactionEffects = new ArrayList<>();
-        eventFactionEffects.add(eventFactionEffect);
-        Season season = Season.spring;
-        String eventDescription = "lorem ipsum";
-
-        Event event = new Event(season, eventDescription, eventFactionEffects);
-
         Assertions.assertThat(event.getEventDetails()).isEqualTo(eventDescription);
-
     }
 
     @Test
-    public void getFactionEffects() {
-        EventFactionEffect eventFactionEffect = new EventFactionEffect(FactionType.capitalist, 1, 2);
-        EventFactionEffect eventFactionEffect2 = new EventFactionEffect(null, 1, 2);
-        List<EventFactionEffect> eventFactionEffects = new ArrayList<>();
-        eventFactionEffects.add(eventFactionEffect);
-        eventFactionEffects.add(eventFactionEffect2);
-        Season season = Season.spring;
-        String eventDescription = "lorem ipsum";
+    public void getEventChoices() {
 
-        Event event = new Event(season, eventDescription, eventFactionEffects);
+        Event event = new Event(season, eventDescription, eventChoices);
 
-        Assertions.assertThat(event.getFactionEffects()).hasSize(2).containsAll(eventFactionEffects);
+        Assertions.assertThat(event.getEventChoices()).hasSize(1).containsAll(eventChoices);
     }
 
     @Test
     public void testToString() {
-        EventFactionEffect eventFactionEffect = new EventFactionEffect(FactionType.capitalist, 1, 2);
-        EventFactionEffect eventFactionEffect2 = new EventFactionEffect(null, 1, 2);
-        List<EventFactionEffect> eventFactionEffects = new ArrayList<>();
-        eventFactionEffects.add(eventFactionEffect);
-        eventFactionEffects.add(eventFactionEffect2);
-        Season season = Season.spring;
-        String eventDescription = "lorem ipsum";
 
-        String expectedString = eventDescription + "\nEffet(s) :\n" +
-                eventFactionEffects.stream()
-                        .map(o -> "- " + o.toString())
-                        .collect(Collectors.joining("\n"));
+        String expectedString = "lorem ipsum\n"
+                + "Choix :\n" +
+                "- choix\n" +
+                "  Effets :\n" +
+                "  - Capitaliste : +1% de partisans, +2% de satisfaction\n" +
+                "  - Toute les factions : +1% de partisans, +2% de satisfaction";
 
-        Event event = new Event(season, eventDescription, eventFactionEffects);
+        Event event = new Event(season, eventDescription, eventChoices);
 
         Assertions.assertThat(event.toString()).isEqualTo(expectedString);
     }
