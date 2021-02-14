972cd50d3a3ce44318ba9d3b01149b562d3d274c
Nouu <noelarrieulacoste@yahoo.fr>
2021-02-14 19:01:40 +0100

    complete event choice

--- src/main/java/org/esgi/el_presidente/core/events/EventChoice.java
+++ src/main/java/org/esgi/el_presidente/core/events/EventChoice.java
@@ -1,5 +1,7 @@
 package org.esgi.el_presidente.core.events;
 
+import org.apache.commons.collections4.CollectionUtils;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -7,29 +9,102 @@ import java.util.stream.Collectors;
 
 public class EventChoice {
     private final String choiceName;
+    private final int industryEffect;
+    private final int agricultureEffect;
+    private final int foodEffect;
+    private final int financeEffect;
     private final List<EventFactionEffect> factionEffects;
 
-    public EventChoice(String choiceName, List<EventFactionEffect> factionEffects) {
+
+    /**
+     * Instanciate Event choice
+     *
+     * @param choiceName        choice description
+     * @param industryEffect    industryEffect
+     * @param agricultureEffect agricultureEffect
+     * @param foodEffect        foodEffect
+     * @param financeEffect     financeEffect
+     * @param factionEffects    List<EventFactionEffect>
+     **/
+    public EventChoice(String choiceName, int industryEffect, int agricultureEffect, int foodEffect, int financeEffect,
+                       List<EventFactionEffect> factionEffects) {
         this.choiceName = choiceName;
+        this.industryEffect = industryEffect;
+        this.agricultureEffect = agricultureEffect;
+        this.foodEffect = foodEffect;
+        this.financeEffect = financeEffect;
         this.factionEffects = Objects.requireNonNullElseGet(factionEffects, ArrayList::new);
+    }
+
+    /**
+     * Add faction effect
+     *
+     * @param factionEffect EventFactionEffect
+     * @return current object
+     */
+    public EventChoice addEventFactionEffect(EventFactionEffect factionEffect) {
+        this.factionEffects.add(factionEffect);
+        return this;
     }
 
     public String getChoiceName() {
         return choiceName;
     }
 
+    public int getIndustryEffect() {
+        return industryEffect;
+    }
+
+    public int getAgricultureEffect() {
+        return agricultureEffect;
+    }
+
+    public int getFoodEffect() {
+        return foodEffect;
+    }
+
+    public int getFinanceEffect() {
+        return financeEffect;
+    }
+
     public List<EventFactionEffect> getFactionEffects() {
         return factionEffects;
     }
 
-    public EventChoice addEventFactionEffect(EventFactionEffect factionEffect) {
-        this.factionEffects.add(factionEffect);
-        return this;
-    }
-
     @Override
     public String toString() {
-        return choiceName + "\n  Effets :\n" +
-                factionEffects.stream().map(f -> "  - " + f.toString()).collect(Collectors.joining("\n"));
+        StringBuilder string = new StringBuilder();
+        string.append(choiceName).append("\n")
+                .append("  Effets :");
+
+        if (!CollectionUtils.isEmpty(factionEffects)) {
+            string.append("\n");
+            string.append(factionEffects.stream().map(f -> "  - " + f.toString()).collect(Collectors.joining("\n")));
+        }
+
+        if (industryEffect > 0) {
+            string.append("\n  - ").append("+").append(industryEffect).append("% d'industrie");
+        } else if (industryEffect < 0) {
+            string.append("\n  - ").append(industryEffect).append("% d'industrie");
+        }
+        if (agricultureEffect > 0) {
+            string.append("\n  - ").append("+").append(agricultureEffect).append("% d'agriculture");
+        } else if (agricultureEffect < 0) {
+            string.append("\n  - ").append(agricultureEffect).append("% d'agriculture");
+        }
+
+        if (foodEffect > 0) {
+            string.append("\n  - ").append("+").append(foodEffect).append(" nourriture");
+        } else if (foodEffect < 0) {
+            string.append("\n  - ").append(foodEffect).append(" nourriture");
+
+        }
+        if (financeEffect > 0) {
+            string.append("\n  - ").append("+").append(financeEffect).append("$");
+        } else if (financeEffect < 0) {
+            string.append("\n  - ").append(financeEffect).append("$");
+        }
+
+        return string.toString();
     }
 }
