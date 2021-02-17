5e52babc793984104f6a10a067d66413c64be79c
Noe Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2021-02-17 16:32:46 +0100

    scenarioList test

---
+++ src/test/java/org/esgi/el_presidente/core/scenario/ScenarioListTest.java
@@ -0,0 +1,31 @@
+package org.esgi.el_presidente.core.scenario;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class ScenarioListTest {
+
+    @Test
+    public void testSandBoxExists() {
+        Assertions.assertThat(ScenarioList.valueOf("SANDBOX")).isNotNull();
+    }
+
+    @Test
+    public void getName() {
+        Assertions.assertThat(ScenarioList.SANDBOX.getName()).isEqualTo("Bac à sable");
+    }
+
+    @Test
+    public void getPath() {
+        Assertions.assertThat(ScenarioList.SANDBOX.getPath()).isEqualTo("sandbox.json");
+    }
+
+    @Test
+    public void testToString() {
+        Arrays.stream(ScenarioList.values()).forEach(s -> Assertions.assertThat(s.toString()).isEqualTo(s.getName()));
+    }
+}
