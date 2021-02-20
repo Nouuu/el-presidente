988c1f1c6cceb0bdcf7d1b232062e9e0280fda31
Swann <swann@devloup.dev>
2021-02-20 23:24:35 +0100

    test year count

--- src/main/java/org/esgi/el_presidente/App.java
+++ src/main/java/org/esgi/el_presidente/App.java
@@ -3,17 +3,7 @@ package org.esgi.el_presidente;
 import org.esgi.el_presidente.javafx.FxApp;
 
 public class App {
-
     public static void main(String[] args) {
         FxApp.launchApp();
     }
-
 }
-
-/*
- * EventManager eventsManager =
- * EventManager.getEventManagerFromJson("eventManager1.json");
- *
- * System.out.println( eventsManager.getEvents() .stream() .map(Event::toString)
- * .collect(Collectors.joining("\n")) );
- */
