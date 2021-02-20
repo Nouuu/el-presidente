0c18093dd947f2524b3bcea09248be91cc8dcd44
Swann <swann@devloup.dev>
2021-02-21 00:06:01 +0100

    test game

--- src/test/java/org/esgi/el_presidente/core/game/GameSteps.java
+++ src/test/java/org/esgi/el_presidente/core/game/GameSteps.java
@@ -1,5 +1,78 @@
 package org.esgi.el_presidente.core.game;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import org.esgi.el_presidente.core.events.Event;
+import org.esgi.el_presidente.core.scenario.Scenario;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class GameSteps {
-  
+  private Game game;
+  private boolean isEndOfYear;
+
+  @Given("a game with test scenario in easy")
+  public void init_with_test_scenario() throws JsonProcessingException {
+    Difficulty difficulty = Difficulty.EASY;
+    Scenario scenario = Scenario.createFromJson("test/scenarioTest.json");
+    game = new Game(scenario, difficulty);
+  }
+
+  @When("i check end of year")
+  public void checkEndOfYear() {
+    isEndOfYear = game.isTheEndOfTheYear();
+  }
+
+  @When("i go to the next turn")
+  public void goToTheNextTurn() {
+    game.nextTurn();
+  }
+
+  @Then("it's the end of the year")
+  public void itsTheEndOfYear() {
+    assertEquals(true, isEndOfYear);
+  }
+
+  @Then("it's not the end of the year")
+  public void itsNotTheEndOfYear() {
+    assertEquals(false, isEndOfYear);
+  }
+
+  @Then("it's lost")
+  public void itsLost() {
+    assertEquals(false, game.isNotLost());
+  }
+
+  @Then("it's not lost")
+  public void itsNotLost() {
+    assertEquals(true, game.isNotLost());
+  }
+
+  @Then("the current event should be {string}")
+  public void eventIs(String eventName) throws Exception {
+    Event expectedEvent = getEventFromString(eventName);
+    Event currentEvent = game.getCurrentEvent();
+    assertEquals(expectedEvent.toString(), currentEvent.toString());
+  }
+
+  private Event getEventFromString(String eventName) throws Exception {
+    switch (eventName) {
+      case "eventTest":
+        return Event.createFromJson("test/eventTest.json");
+      case "eventTest2":
+        return Event.createFromJson("test/eventTest2.json");
+      case "eventTestError":
+        return Event.createFromJson("test/eventTest2.json");
+      case "eventTestError2":
+        return Event.createFromJson("test/eventTest2.json");
+
+      default:
+        throw new Exception("event is not in the list");
+    }
+  }
 }
--- src/test/java/org/esgi/el_presidente/core/game/game.feature
+++ src/test/java/org/esgi/el_presidente/core/game/game.feature
@@ -1,7 +1,9 @@
 Feature: Game
 
-Loop
-event
-saison
-fin d'année
-fin du jeu
+  Scenario: test game in scenario
+    Given a game with test scenario in easy
+    When i check end of year
+    Then it's not the end of the year
+    And it's not lost
+    When i go to the next turn
+    And the current event should be "eventTest"
