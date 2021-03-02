e3ee77c096f866d46179fd004b1f835738f13c54
Swann <swann@devloup.dev>
2021-03-02 20:55:21 +0100

    update CLI to ask for difficulty and scenario

--- src/main/java/org/esgi/el_presidente/cli/Cli.java
+++ src/main/java/org/esgi/el_presidente/cli/Cli.java
@@ -3,20 +3,28 @@ package org.esgi.el_presidente.cli;
 import java.util.List;
 import java.util.Scanner;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 import org.esgi.el_presidente.core.events.Event;
 import org.esgi.el_presidente.core.events.EventChoice;
 import org.esgi.el_presidente.core.factions.Faction;
 import org.esgi.el_presidente.core.factions.FactionManager;
 import org.esgi.el_presidente.core.factions.FactionType;
+import org.esgi.el_presidente.core.game.Difficulty;
 import org.esgi.el_presidente.core.game.Game;
 import org.esgi.el_presidente.core.ressources.RessourceManager;
+import org.esgi.el_presidente.core.scenario.Scenario;
+import org.esgi.el_presidente.core.scenario.ScenarioList;
 
 public class Cli {
 
   private Game game;
 
-  public Cli(Game game) {
-    this.game = game;
+  public Cli() throws JsonProcessingException {
+    Scenario scenario = selectScenario();
+    Difficulty difficulty = selectGameMode();
+    System.out.println(scenario.getIntroduction() + " IN " + difficulty.name());
+    this.game = new Game(scenario, difficulty);
   }
 
   public void loop() {
@@ -37,6 +45,37 @@ public class Cli {
         System.out.println(reviewTheGame());
       }
     }
+  }
+
+  private Difficulty selectGameMode() {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Select Game Mode: 1 for EASY 2 for NORMAL 3 press 4 if you are JDG");
+    int choice = input.nextInt();
+    switch (choice) {
+      case 1:
+        return Difficulty.EASY;
+      case 2:
+        return Difficulty.MEDIUM;
+      case 3:
+        return Difficulty.HARD;
+      case 4:
+        return Difficulty.HARDCORE;
+      default:
+        return Difficulty.MEDIUM;
+    }
+  }
+
+  private Scenario selectScenario() throws JsonProcessingException {
+    Scanner input = new Scanner(System.in);
+    ScenarioList[] scenarioList = ScenarioList.values();
+    System.out.println("Veuillez chosir un scenario");
+    for (int i = 0; i < scenarioList.length; i++) {
+      System.out.println(i + ": " + scenarioList[i]);
+    }
+
+    int choice = input.nextInt();
+    Scenario scenario = Scenario.createFromJson(scenarioList[choice].getPath());
+    return scenario;
   }
 
   private void displayEndOfYearBilan() {
@@ -74,7 +113,7 @@ public class Cli {
       if (factionType != null) {
         faction = factionManager.getFaction(factionType);
         try {
-          ressourceManager.buyBribe(faction);// TODO Ce n'est pas a ressourceManager de faire ça
+          ressourceManager.buyBribe(faction);
         } catch (Exception exception) {
           System.out.println(exception.getMessage());
         }
@@ -111,9 +150,10 @@ public class Cli {
     FactionManager factionManager = game.getFactionManager();
     StringBuilder gameStrBuilder = new StringBuilder();
 
-    gameStrBuilder.append("GlobalSatisfaction: " + factionManager.getGlobalSatisfaction() + "\n");
+    System.out.println("GlobalSatisfaction: " + factionManager.getGlobalSatisfaction());
     askForFactionsDetails(gameStrBuilder);
 
+    gameStrBuilder.append("GlobalSatisfaction: " + factionManager.getGlobalSatisfaction() + "\n");
     gameStrBuilder.append("Money: " + ressourceManager.getMoney() + "\n");
     gameStrBuilder.append("FoodReseve: " + ressourceManager.getFoodReserves() + "\n");
     gameStrBuilder.append("Agriculture part: " + ressourceManager.getAgriculturePart() + "\n");
